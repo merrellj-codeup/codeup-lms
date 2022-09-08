49832f884afbf5ac07c2db15dd07c016a498a2fa
merrellj-codeup <jason.merrell@codeup.com>
2022-09-08 11:32:50 -0500

    login form validation

---
+++ Login/index.js
@@ -0,0 +1,18 @@
+$('#login').on('click', function() {
+    $("#loginForm").validate();
+    var email = $('#Email').val();
+    console.log(username + ' is trying to login.');
+});
+
+function loginFirebaseUser(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, 'yellowstone')
+        .then((userCredential) => {
+        // Signed in
+        console.log('User is logged into Firebase.')
+        window.location.replace('/app/dashboard');
+        })
+        .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        });
+}
