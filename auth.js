569e585036406d0c089c0c741b5786f5f6276f28
merrellj-codeup <jason.merrell@codeup.com>
2022-09-09 14:50:15 -0500

    cohort data trigger

--- auth.js
+++ auth.js
@@ -60,7 +60,7 @@ function getCohortData() {
       .then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
               // doc.data() is never undefined for query doc snapshots
-              var cohortID = doc.data();
+              var cohortID = doc.id;
               cohorts[cohortID] = {
                 'class_code': doc.data().class_code
               };
