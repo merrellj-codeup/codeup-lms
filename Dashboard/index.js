21152582f653781c8fd40b5a04b76b701b184904
merrellj-codeup <jason.merrell@codeup.com>
2022-09-12 16:55:40 -0500

    cohort data

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 
 $(document).on('cohortData', function(){
-  $('#cohort_status').find('.grid-box-quantifier').first().text( (cohort.status && cohort.status) );
+  $('#cohort_status').find('.grid-box-quantifier').first().text( cohort.status );
 });
 
 $(document).on('pageReady', function(){
