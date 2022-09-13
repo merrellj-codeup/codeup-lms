b8fc949a5d6a44707c68fad09a749c5ac4b97893
merrellj-codeup <jason.merrell@codeup.com>
2022-09-13 15:37:59 -0500

    google auth

--- Login/index.js
+++ Login/index.js
@@ -1,3 +1,5 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
 $(document).ready(function(){
     $('#Email').val('jason.merrell@codeup.com');
     $('#password').val('abc123');
@@ -15,6 +17,30 @@ $('#login').on('click', function() {
     var password = $('#password').val();
     console.log(email + ' is trying to login.');
     loginFirebaseUser('jason.merrell@codeup.com', 'abc123');
+});
+
+$('#googleAuth').on('click', function(){
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
 });
 
 function loginFirebaseUser(email, password) {
