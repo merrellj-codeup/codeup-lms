0315eff29030841a14043ddac4bd9199deaebff4
merrellj-codeup <jason.merrell@codeup.com>
2022-09-13 15:49:47 -0500

    google auth

--- Login/index.js
+++ Login/index.js
@@ -21,26 +21,7 @@ $('#login').on('click', function() {
 
 $('#googleAuth').on('click', function(){
     firebase.auth()
-        .signInWithRedirect(provider)
-        .then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
+        .signInWithRedirect(provider);
 });
 
 function loginFirebaseUser(email, password) {
