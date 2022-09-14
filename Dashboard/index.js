1b42e6f27c8f4ad6030ee917d6f860f5f4575a7e
merrellj-codeup <jason.merrell@codeup.com>
2022-09-14 15:09:25 -0500

    setting up google classroom api requests

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -13,6 +13,12 @@ $(document).on('cohortData', function(){
     $('#cohort_status').find('.grid-box-quantifier').first().text('Unknown');
     $('#cohort_status').find('.grid-box-quantifier').first().addClass('red');
   }
+  if (cohort.student_count) {
+    $('#studentCount').find('.grid-box-quantifier').first().text( cohort.status );
+    if (cohort.status === "active") {
+      $('#cohort_status').find('.grid-box-quantifier').first().addClass('green');
+    }
+  }
 });
 
 $(document).on('pageReady', function(){
