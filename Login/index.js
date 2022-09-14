f809a277f639036ed1a25b24aab457fca4964b6e
merrellj-codeup <jason.merrell@codeup.com>
2022-09-14 12:48:39 -0500

    google auth

--- Login/index.js
+++ Login/index.js
@@ -1,33 +1,11 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/classroom.rosters.readonly https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.students.readonly');
 
+
+
 $(document).ready(function(){
     $('#Email').val('jason.merrell@codeup.com');
     $('#password').val('abc123');
-
-    firebase.auth()
-        .getRedirectResult()
-        .then((result) => {
-            if (result.credential) {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // ...
-            }
-            // The signed-in user info.
-            console.log(result.user);
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
 });
 
 $('input').keypress(function(event){
@@ -45,7 +23,27 @@ $('#login').on('click', function() {
 });
 
 $('#googleAuth').on('click', function(){
-    firebase.auth().signInWithRedirect(provider);
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+        
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
 });
 
 function loginFirebaseUser(email, password) {
