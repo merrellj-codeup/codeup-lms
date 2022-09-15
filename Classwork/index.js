d771bfb11b1b0084b2062fe2acbe586c320c265e
merrellj-codeup <jason.merrell@codeup.com>
2022-09-15 09:59:56 -0500

    setting up classroom api to display coursework

--- Classwork/index.js
+++ Classwork/index.js
@@ -1,3 +1,34 @@
 $(document).on('pageReady', function(){
     $('.timeline-wrapper').animate({scrollLeft: ( ($('#week5').position().left) - 75 )}, 1000);
 });
+
+$(document).on('pageReady', function(){
+    getCoursework(cohort.classroom_id, user.token);
+});
+
+function getCoursework(courseId, accessToken) {
+    // https://classroom.googleapis.com/v1/courses?teacherId=me
+    const url = `https://classroom.googleapis.com/v1/courses/${courseId}/courseWork`;
+    const options = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+    let coursework = [];
+    fetch(url, options)
+        .then(response => {
+            return response.json();
+        }).then(json => {
+            for (let i = 0; i < json.courseWork.length ; i++) {
+                const cw = {
+                    id: json.courseWork[i].id,
+                    title: json.courseWork[i].title,
+                    dueDate: json.courseWork[i].dueDate,
+                    category: json.courseWork[i].gradeCategory.name
+                }
+                coursework[cw.id] = cw;
+            }
+        });
+    console.log(coursework);
+    return coursework;
+}
