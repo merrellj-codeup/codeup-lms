62fd6d6b3c7abffc4e6c0b93bdc9f3ee40752d89
merrellj-codeup <jason.merrell@codeup.com>
2022-09-15 10:37:42 -0500

    setting up classroom api to display coursework

--- Classwork/index.js
+++ Classwork/index.js
@@ -1,28 +1,23 @@
-"use strict";
-
-(async function() {
-    $(document).on('pageReady', function(){
-        $('.timeline-wrapper').animate({scrollLeft: ( ($('#week5').position().left) - 75 )}, 1000);
-    });
-    
-    $(document).on('cohortData', function(){
-        getTopics(cohort.classroom_id, user.token);
-    });
-    
-    function getTopics(courseId, accessToken) {
-        // https://classroom.googleapis.com/v1/courses?teacherId=me
-        const url = `https://classroom.googleapis.com/v1/courses/${courseId}/topics`;
-        const options = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        };
-        fetch(url, options)
-            .then(response => {
-                return response.json();
-            }).then(json => {
-                console.log(json);
-            });
-        return coursework;
-    }
+$(document).on('pageReady', function(){
+    $('.timeline-wrapper').animate({scrollLeft: ( ($('#week5').position().left) - 75 )}, 1000);
 });
+
+$(document).on('cohortData', function(){
+    getTopics(cohort.classroom_id, user.token);
+});
+
+function getTopics(courseId, accessToken) {
+    // https://classroom.googleapis.com/v1/courses?teacherId=me
+    const url = `https://classroom.googleapis.com/v1/courses/${courseId}/topics`;
+    const options = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+    fetch(url, options)
+        .then(response => {
+            return response.json();
+        }).then(json => {
+            console.log(json);
+        });
+}
