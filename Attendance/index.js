0fc5c83c198eace5a4c658794d29907839b7825c
merrellj-codeup <jason.merrell@codeup.com>
2022-09-26 16:23:24 -0500

    attendance prototyping functionality

--- Attendance/index.js
+++ Attendance/index.js
@@ -17,3 +17,39 @@ $(document).on('click', '.cal-saved-status', function(){
             break;
     }
 });
+
+$(document).on('click', '.cal-event', function(){
+    let currentStatus = $(this).find('.cal-event-status absent').text();
+    $('[data-save="attendance"]').addClass('cta');
+    $(this).parents('.cal-day').find('.cal-saved-status saved').addClass('unsaved');
+    $(this).parents('.cal-day').find('.cal-saved-status saved').children('div').text('Unsaved');
+    switch(currentStatus) {
+        case "P":
+            $(this).html(`<div class="cal-event-status present">PV</div>`);
+            break;
+        case "PV":
+            $(this).html(`<div class="cal-event-status tardy">T</div>`);
+            break;
+        case "T":
+            $(this).html(`<div class="cal-event-status tardy">TV</div>`);
+            break;
+        case "TV":
+            $(this).html(`<div class="cal-event-status tardy">H</div>`);
+            break;
+        case "H":
+            $(this).html(`<div class="cal-event-status tardy">HV</div>`);
+            break;
+        case "HV":
+            $(this).html(`<div class="cal-event-status absent">A</div>`);
+            break;
+        case "A":
+            $(this).html(`<div class="cal-event-status unexcused">UA</div>`);
+            break;
+        case "UA":
+            $(this).html(``);
+            break;
+        default:
+            $(this).html(`<div class="cal-event-status present">P</div>`);
+            break;
+    }
+});
