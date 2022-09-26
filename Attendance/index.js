6fe2986f89d4a37b0c0d9e367f26e3ab7eea32cd
merrellj-codeup <jason.merrell@codeup.com>
2022-09-26 17:21:45 -0500

    attendance prototyping functionality

--- Attendance/index.js
+++ Attendance/index.js
@@ -23,33 +23,33 @@ $(document).on('click', '.cal-event', function(){
     $('[data-save="attendance"]').addClass('cta');
     $(this).parents('.cal-day').find('.cal-saved-status').addClass('unsaved');
     $(this).parents('.cal-day').find('.cal-saved-status').children('div').text('Unsaved');
-    switch(currentStatus) {
-        case "P":
-            $(this).html(`<div class="cal-event-status present">PV</div>`);
-            break;
-        case "PV":
-            $(this).html(`<div class="cal-event-status tardy">T</div>`);
-            break;
-        case "T":
-            $(this).html(`<div class="cal-event-status tardy">TV</div>`);
-            break;
-        case "TV":
-            $(this).html(`<div class="cal-event-status tardy">H</div>`);
-            break;
-        case "H":
-            $(this).html(`<div class="cal-event-status tardy">HV</div>`);
-            break;
-        case "HV":
-            $(this).html(`<div class="cal-event-status absent">A</div>`);
-            break;
-        case "A":
-            $(this).html(`<div class="cal-event-status unexcused">UA</div>`);
-            break;
-        case "UA":
-            $(this).html(``);
-            break;
-        default:
-            $(this).html(`<div class="cal-event-status present">P</div>`);
-            break;
-    }
+    // switch(currentStatus) {
+    //     case "P":
+    //         $(this).html(`<div class="cal-event-status present">PV</div>`);
+    //         break;
+    //     case "PV":
+    //         $(this).html(`<div class="cal-event-status tardy">T</div>`);
+    //         break;
+    //     case "T":
+    //         $(this).html(`<div class="cal-event-status tardy">TV</div>`);
+    //         break;
+    //     case "TV":
+    //         $(this).html(`<div class="cal-event-status tardy">H</div>`);
+    //         break;
+    //     case "H":
+    //         $(this).html(`<div class="cal-event-status tardy">HV</div>`);
+    //         break;
+    //     case "HV":
+    //         $(this).html(`<div class="cal-event-status absent">A</div>`);
+    //         break;
+    //     case "A":
+    //         $(this).html(`<div class="cal-event-status unexcused">UA</div>`);
+    //         break;
+    //     case "UA":
+    //         $(this).html(``);
+    //         break;
+    //     default:
+    //         $(this).html(`<div class="cal-event-status present">P</div>`);
+    //         break;
+    // }
 });
