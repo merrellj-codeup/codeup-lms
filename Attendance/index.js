de991c5176c5c09ab632a3e06cd4066f73f507ee
merrellj-codeup <jason.merrell@codeup.com>
2022-09-27 09:37:51 -0500

    attendance prototyping functionality

--- Attendance/index.js
+++ Attendance/index.js
@@ -23,35 +23,49 @@ $(document).on('click', '.cal-event', function(){
     $('[data-save="attendance"]').addClass('cta');
     $(this).parents('.cal-day').find('.cal-saved-status').addClass('unsaved');
     $(this).parents('.cal-day').find('.cal-saved-status').children('div').text('Unsaved');
-    // switch(currentStatus) {
-    //     case "P":
-    //         $(this).html(`<div class="cal-event-status present">PV</div>`);
-    //         break;
-    //     case "PV":
-    //         $(this).html(`<div class="cal-event-status tardy">T</div>`);
-    //         break;
-    //     case "T":
-    //         $(this).html(`<div class="cal-event-status tardy">TV</div>`);
-    //         break;
-    //     case "TV":
-    //         $(this).html(`<div class="cal-event-status tardy">H</div>`);
-    //         break;
-    //     case "H":
-    //         $(this).html(`<div class="cal-event-status tardy">HV</div>`);
-    //         break;
-    //     case "HV":
-    //         $(this).html(`<div class="cal-event-status absent">A</div>`);
-    //         break;
-    //     case "A":
-    //         $(this).html(`<div class="cal-event-status unexcused">UA</div>`);
-    //         break;
-    //     case "UA":
-    //         $(this).html(``);
-    //         break;
-    //     default:
-    //         $(this).html(`<div class="cal-event-status present">P</div>`);
-    //         break;
-    // }
+
+    let attendanceWindow = `
+    <div class="attendance-window-container">
+        <div class="attendance-window-bg"></div>
+        <div class="attendance-status-window">
+            <div class="attendance-status-window-inner">
+                <div class="div-block-12">
+                    <div class="cal-event-status present">P</div>
+                    <div>Present</div>
+                </div>
+                <div class="div-block-12">
+                    <div class="cal-event-status tardy">T</div>
+                    <div>Tardy</div>
+                </div>
+                <div class="div-block-12">
+                    <div class="cal-event-status tardy">H</div>
+                    <div>Half-Day</div>
+                </div>
+                <div class="div-block-12">
+                    <div class="cal-event-status tardy">L</div>
+                    <div>Left Early</div>
+                </div>
+                <div class="div-block-12">
+                    <div class="cal-event-status absent">A</div>
+                    <div>Absent</div>
+                </div>
+                <div class="div-block-12">
+                    <div class="cal-event-status unexcused">UA</div>
+                    <div>Unexcused Absence</div>
+                </div>
+                <div class="div-block-12 virtual">
+                    <div>Virtual?</div>
+                    <div data-toggle="parent" class="toggle">
+                        <div data-toggle="wrapper" class="toggle-button-wrapper">
+                            <div data-toggle="button" class="toggle-button"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    $(attendanceWindow).appendAfter('.main');
 });
 
 $(document).on('click', '.attendance-window-bg', function(){
