fe78d65a35876f9e7e20a60a375779e1c183d050
merrellj-codeup <jason.merrell@codeup.com>
2022-09-27 11:18:44 -0500

    attendance prototyping functionality

--- Attendance/index.js
+++ Attendance/index.js
@@ -1,4 +1,37 @@
 let $clickedCalEvent = {};
+$(document).ready(function(){
+    const ctx = document.getElementById('myChart');
+    const DATA_COUNT = 5;
+    const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+    const data = {
+        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+        datasets: [
+            {
+            label: 'Dataset 1',
+            data: Utils.numbers(NUMBER_CFG),
+            backgroundColor: Object.values(Utils.CHART_COLORS),
+            }
+        ]
+    };
+    const config = {
+        type: 'pie',
+        data: data,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Pie Chart'
+            }
+          }
+        },
+    };
+    const myChart = new Chart(ctx, config);
+});
 $(document).on('click', '.cal-saved-status', function(){
     let currentStatus = $(this).text();
     switch(currentStatus){
